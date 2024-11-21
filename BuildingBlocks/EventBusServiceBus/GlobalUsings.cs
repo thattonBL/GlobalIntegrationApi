5879478ce98ba7eca517ef527779e65bdc343c31
thatton <thomasrichardhatton@googlemail.com>
2024-11-21 16:56:53 +0000

    Adding Library Dependencies
    - Need to find a better way but just adding these here for now

---
+++ BuildingBlocks/EventBusServiceBus/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Text;
+global using System.Text.Json;
+global using Azure.Messaging.ServiceBus;
+global using Azure.Messaging.ServiceBus.Administration;
+global using Microsoft.Extensions.Logging;
